b94910e5924d98ea9cc5884a978c0f28ffe704a0
James Hilliard <james.hilliard1@gmail.com>
2019-09-29 05:12:12 -0600

    package/cmake: add version 3.15.3 and move 3.9.6 to cmake-compat
    Retain cmake 3.9.6 for compatibility with toolchains without c++11 support.
    
    Autodetect and build using latest compatible host-cmake.
    
    Remove FindLibUV patch which is now upstream.
    
    Resync/reformat rename cmake rootfile patch for 3.15.3 with git format-patch.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Reviewed-by: Adrian Perez de Castro <aperez@igalia.com>
    
    ---
    Changes v4 -> v5:
      - create separate cmake packages
    
    Changes v3 -> v4:
      - retain support for version 3.8.2 for older toolchains
    
    Changes v2 -> v3:
      - split minimum cmake off from cmake version bump
    
    Changes v1 -> v2:
      - Set cmake version min to 3.10.

--- package/cmake-compat/cmake-compat.mk
+++ package/cmake-compat/cmake-compat.mk
@@ -1,14 +1,14 @@
 ################################################################################
 #
-# cmake
+# cmake-compat
 #
 ################################################################################
 
-CMAKE_VERSION_MAJOR = 3.15
-CMAKE_VERSION = $(CMAKE_VERSION_MAJOR).3
-CMAKE_SITE = https://cmake.org/files/v$(CMAKE_VERSION_MAJOR)
-CMAKE_LICENSE = BSD-3-Clause
-CMAKE_LICENSE_FILES = Copyright.txt
+CMAKE_COMPAT_VERSION_MAJOR = 3.9
+CMAKE_COMPAT_VERSION = $(CMAKE_COMPAT_VERSION_MAJOR).6
+CMAKE_COMPAT_SITE = https://cmake.org/files/v$(CMAKE_COMPAT_VERSION_MAJOR)
+CMAKE_COMPAT_LICENSE = BSD-3-Clause
+CMAKE_COMPAT_LICENSE_FILES = Copyright.txt
 
 # CMake is a particular package:
 # * CMake can be built using the generic infrastructure or the cmake one.
@@ -22,9 +22,9 @@ CMAKE_LICENSE_FILES = Copyright.txt
 #   the system-wide libraries instead of rebuilding and statically
 #   linking with the ones bundled into the CMake sources.
 
-CMAKE_DEPENDENCIES = zlib jsoncpp libcurl libarchive expat bzip2 xz libuv rhash
+CMAKE_COMPAT_DEPENDENCIES = zlib jsoncpp libcurl libarchive expat bzip2 xz libuv rhash
 
-CMAKE_CONF_OPTS = \
+CMAKE_COMPAT_CONF_OPTS = \
 	-DKWSYS_LFS_WORKS=TRUE \
 	-DKWSYS_CHAR_IS_SIGNED=TRUE \
 	-DCMAKE_USE_SYSTEM_LIBRARIES=1 \
@@ -34,51 +34,29 @@ CMAKE_CONF_OPTS = \
 # Get rid of -I* options from $(HOST_CPPFLAGS) to prevent that a
 # header available in $(HOST_DIR)/include is used instead of a
 # CMake internal header, e.g. lzma* headers of the xz package
-HOST_CMAKE_CFLAGS = $(shell echo $(HOST_CFLAGS) | sed -r "s%$(HOST_CPPFLAGS)%%")
-HOST_CMAKE_CXXFLAGS = $(shell echo $(HOST_CXXFLAGS) | sed -r "s%$(HOST_CPPFLAGS)%%")
+HOST_CMAKE_COMPAT_CFLAGS = $(shell echo $(HOST_CFLAGS) | sed -r "s%$(HOST_CPPFLAGS)%%")
+HOST_CMAKE_COMPAT_CXXFLAGS = $(shell echo $(HOST_CXXFLAGS) | sed -r "s%$(HOST_CPPFLAGS)%%")
 
-define HOST_CMAKE_CONFIGURE_CMDS
+define HOST_CMAKE_COMPAT_CONFIGURE_CMDS
 	(cd $(@D); \
 		$(HOST_CONFIGURE_OPTS) \
-		CFLAGS="$(HOST_CMAKE_CFLAGS)" \
+		CFLAGS="$(HOST_CMAKE_COMPAT_CFLAGS)" \
 		./bootstrap --prefix=$(HOST_DIR) \
 			--parallel=$(PARALLEL_JOBS) -- \
-			-DCMAKE_C_FLAGS="$(HOST_CMAKE_CFLAGS)" \
-			-DCMAKE_CXX_FLAGS="$(HOST_CMAKE_CXXFLAGS)" \
+			-DCMAKE_C_FLAGS="$(HOST_CMAKE_COMPAT_CFLAGS)" \
+			-DCMAKE_CXX_FLAGS="$(HOST_CMAKE_COMPAT_CXXFLAGS)" \
 			-DCMAKE_EXE_LINKER_FLAGS="$(HOST_LDFLAGS)" \
 			-DCMAKE_USE_OPENSSL:BOOL=OFF \
 			-DBUILD_CursesDialog=OFF \
 	)
 endef
 
-define HOST_CMAKE_BUILD_CMDS
+define HOST_CMAKE_COMPAT_BUILD_CMDS
 	$(HOST_MAKE_ENV) $(MAKE) -C $(@D)
 endef
 
-define HOST_CMAKE_INSTALL_CMDS
+define HOST_CMAKE_COMPAT_INSTALL_CMDS
 	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) install/fast
 endef
 
-define CMAKE_REMOVE_EXTRA_DATA
-	rm $(TARGET_DIR)/usr/bin/{cmake,cpack}
-	rm -fr $(TARGET_DIR)/usr/share/cmake-$(CMAKE_VERSION_MAJOR)/{completions,editors}
-	rm -fr $(TARGET_DIR)/usr/share/cmake-$(CMAKE_VERSION_MAJOR)/{Help,include}
-endef
-
-define CMAKE_INSTALL_CTEST_CFG_FILE
-	$(INSTALL) -m 0644 -D $(@D)/Modules/CMake.cmake \
-		$(TARGET_DIR)/usr/share/cmake-$(CMAKE_VERSION_MAJOR)/Modules/CMake.cmake.ctest
-endef
-
-CMAKE_POST_INSTALL_TARGET_HOOKS += CMAKE_REMOVE_EXTRA_DATA
-CMAKE_POST_INSTALL_TARGET_HOOKS += CMAKE_INSTALL_CTEST_CFG_FILE
-
-define CMAKE_INSTALL_TARGET_CMDS
-	(cd $(@D); \
-		$(HOST_MAKE_ENV) DESTDIR=$(TARGET_DIR) \
-		cmake -P cmake_install.cmake \
-	)
-endef
-
-$(eval $(cmake-package))
 $(eval $(host-generic-package))
--- support/dependencies/check-host-cmake.mk
+++ support/dependencies/check-host-cmake.mk
@@ -10,5 +10,9 @@ BR2_CMAKE ?= $(call suitable-host-package,cmake,\
 	$(BR2_CMAKE_VERSION_MIN) $(BR2_CMAKE_CANDIDATES))
 ifeq ($(BR2_CMAKE),)
 BR2_CMAKE = $(HOST_DIR)/bin/cmake
+ifeq ($(BR2_HOST_GCC_AT_LEAST_4_8),)
+BR2_CMAKE_HOST_DEPENDENCY = host-cmake-compat
+else
 BR2_CMAKE_HOST_DEPENDENCY = host-cmake
+endif
 endif
