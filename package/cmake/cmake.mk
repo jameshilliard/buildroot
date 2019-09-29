5b963a38aa7f748bd74a2936e29e42040c65d7c9
James Hilliard <james.hilliard1@gmail.com>
2019-09-29 05:12:12 -0600

    cmake: add version 3.15.3
    Retain cmake 3.8.2 for compatibility with toolchains without c++11 support.
    
    Remove FindLibUV patch which is now upstream.
    
    Resync/reformat rename cmake rootfile patch for 3.15.3 with git format-patch.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Reviewed-by: Adrian Perez de Castro <aperez@igalia.com>
    
    ---
    Changes v3 -> v4:
      - retain support for version 3.8.2 for older toolchains
    
    Changes v2 -> v3:
      - split minimum cmake off from cmake version bump
    
    Changes v1 -> v2:
      - Set cmake version min to 3.10.

--- package/cmake/cmake.mk
+++ package/cmake/cmake.mk
@@ -4,8 +4,13 @@
 #
 ################################################################################
 
+ifeq ($(BR2_HOST_GCC_AT_LEAST_4_8),y)
+CMAKE_VERSION_MAJOR = 3.15
+CMAKE_VERSION = $(CMAKE_VERSION_MAJOR).3
+else
 CMAKE_VERSION_MAJOR = 3.8
 CMAKE_VERSION = $(CMAKE_VERSION_MAJOR).2
+endif
 CMAKE_SITE = https://cmake.org/files/v$(CMAKE_VERSION_MAJOR)
 CMAKE_LICENSE = BSD-3-Clause
 CMAKE_LICENSE_FILES = Copyright.txt
