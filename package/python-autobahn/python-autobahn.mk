017a1f52b0c1029a68df412be26cb1419449b36a
James Hilliard <james.hilliard1@gmail.com>
2020-07-07 15:28:47 -0600

    package/python-autobahn: bump to version 20.6.2
    Strip xbr contracts which are used only for the xbr feature which is
    entirely unsupported by buildroot.
    
    Enable the optimized nvx cffi extension module.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>

--- package/python-autobahn/python-autobahn.mk
+++ package/python-autobahn/python-autobahn.mk
@@ -4,11 +4,19 @@
 #
 ################################################################################
 
-PYTHON_AUTOBAHN_VERSION = 20.4.3
+PYTHON_AUTOBAHN_VERSION = 20.6.2
 PYTHON_AUTOBAHN_SOURCE = autobahn-$(PYTHON_AUTOBAHN_VERSION).tar.gz
-PYTHON_AUTOBAHN_SITE = https://files.pythonhosted.org/packages/cb/16/38d27874ef827974f44acb6dd64a92a9248b624734c0e84b91083c2d9350
+PYTHON_AUTOBAHN_SITE = https://files.pythonhosted.org/packages/43/ba/926d323decdce0341f6f60dce45ea5553b08113dddb9ca482554d3db11eb
 PYTHON_AUTOBAHN_LICENSE = MIT
 PYTHON_AUTOBAHN_LICENSE_FILES = LICENSE
 PYTHON_AUTOBAHN_SETUP_TYPE = setuptools
+PYTHON_AUTOBAHN_DEPENDENCIES = host-python-cffi
+PYTHON_AUTOBAHN_ENV = AUTOBAHN_USE_NVX=1 AUTOBAHN_STRIP_XBR=1
+
+define PYTHON_AUTOBAHN_STRIP_XBR_CONTRACTS
+	rm -rf $(@D)/autobahn/xbr/contracts
+endef
+
+PYTHON_AUTOBAHN_POST_EXTRACT_HOOKS = PYTHON_AUTOBAHN_STRIP_XBR_CONTRACTS
 
 $(eval $(python-package))
