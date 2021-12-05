9dc82c69b958ab8a561227291446c3768a0904ff
James Hilliard <james.hilliard1@gmail.com>
2021-12-04 17:35:10 -0700

    package/network-manager: add optional iwd support
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>

--- package/network-manager/network-manager.mk
+++ package/network-manager/network-manager.mk
@@ -42,6 +42,13 @@ else
 NETWORK_MANAGER_CONF_OPTS += --without-ofono
 endif
 
+ifeq ($(BR2_PACKAGE_IWD),y)
+NETWORK_MANAGER_DEPENDENCIES += iwd
+NETWORK_MANAGER_CONF_OPTS += --with-iwd=yes
+else
+NETWORK_MANAGER_CONF_OPTS += --with-iwd=no
+endif
+
 ifeq ($(BR2_PACKAGE_LIBCURL),y)
 NETWORK_MANAGER_DEPENDENCIES += libcurl
 NETWORK_MANAGER_CONF_OPTS += --enable-concheck
