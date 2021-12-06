8beef6927f6659749fda23a0f67b06e4ed1261d2
Guillaume Ayoub <guillaume@courtbouillon.org>
2021-12-06 09:47:54 +0100

    Work in progress

--- package/python-flit-core/python-flit-core.mk
+++ package/python-flit-core/python-flit-core.mk
@@ -5,30 +5,10 @@
 ################################################################################
 
 PYTHON_FLIT_CORE_VERSION = 3.5.1
-PYTHON_FLIT_CORE_SOURCE = flit_core-$(PYTHON_FLIT_CORE_VERSION).tar.gz
-PYTHON_FLIT_CORE_SITE = https://files.pythonhosted.org/packages/7e/1e/15198966abf00e590ec95fb8aa4ba3d274897fe7b182fce2867f672f6a91
-PYTHON_FLIT_CORE_SETUP_TYPE = flit
+PYTHON_FLIT_CORE_SOURCE = flit_core-$(PYTHON_FLIT_CORE_VERSION)-py3-none-any.whl
+PYTHON_FLIT_CORE_SITE = https://files.pythonhosted.org/packages/32/56/f3e180eea51e3e085b1da06233f6e6aa9adb04000d19ba91c29e44823bf7
+PYTHON_FLIT_CORE_SETUP_TYPE = wheel
 PYTHON_FLIT_CORE_LICENSE = BSD-3-Clause
 HOST_PYTHON_FLIT_CORE_NEEDS_HOST_PYTHON = python3
-HOST_PYTHON_FLIT_CORE_DEPENDENCIES = host-python-flit
-HOST_PYTHON_FLIT_CORE_ENV = PYTHONPATH="$(PYTHON3_PATH):$(@D)"
-
-define HOST_PYTHON_FLIT_CORE_FLIT_GENERATE_SETUP
-	cd $(HOST_PYTHON_FLIT_CORE_BUILDDIR)/; \
-		$(HOST_PYTHON_FLIT_CORE_BASE_ENV) $(HOST_PYTHON_FLIT_CORE_ENV) \
-		$(HOST_PYTHON_FLIT_CORE_PYTHON_INTERPRETER) -c \
-		"import sys; \
-		sys.modules['requests'] = False; \
-		from flit.sdist import SdistBuilder; \
-		from flit_core.build_thyself import metadata, Module; \
-		from pathlib import Path; \
-		cwd=Path.cwd(); \
-		module=Module('flit_core', cwd); \
-		reqs_by_extra={'.none': metadata.requires}; \
-		extra_files=['pyproject.toml', 'build_dists.py']; \
-		setup=cwd.joinpath('setup.py').open('wb'); \
-		builder=SdistBuilder(module, metadata, cwd, reqs_by_extra, {}, extra_files); \
-		setup.write(builder.make_setup_py())"
-endef
 
 $(eval $(host-python-package))
