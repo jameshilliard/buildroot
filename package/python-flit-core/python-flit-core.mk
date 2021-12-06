89a465803822e44bf9c96376d3eb058194f1d826
James Hilliard <james.hilliard1@gmail.com>
2021-12-06 06:59:05 -0700

    package/python-flit-core: new package/infrastructure
    This adds pep517(needed for flit-core to build itself) and flit python
    package types.
    
    We need to add an installer script and pass it appropriate options for
    installing pep517 wheels generated by python-pypa-build during the
    build stage. Unfortunately it seems pep517 does not support builds
    without using the wheel format.
    
    We also need to add a patch fixing the version parser in flit-core.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>

---
+++ package/python-flit-core/python-flit-core.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# python-flit-core
+#
+################################################################################
+
+PYTHON_FLIT_CORE_VERSION = 3.6.0
+PYTHON_FLIT_CORE_SOURCE = flit_core-$(PYTHON_FLIT_CORE_VERSION).tar.gz
+PYTHON_FLIT_CORE_SITE = https://files.pythonhosted.org/packages/08/e9/0653f7783ba2ec2f954f19442878427f1d5bfccb01842d354453c2809b22
+PYTHON_FLIT_CORE_SETUP_TYPE = pep517
+PYTHON_FLIT_CORE_LICENSE = BSD-3-Clause
+HOST_PYTHON_FLIT_CORE_NEEDS_HOST_PYTHON = python3
+HOST_PYTHON_FLIT_CORE_DEPENDENCIES = host-python-pypa-build
+
+$(eval $(host-python-package))
