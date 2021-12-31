217d616adc4d92089c65ac1fe52a5cbb0cd81602
Adam Duskett <aduskett@gmail.com>
2021-12-31 15:42:37 -0800

    qt5 kde testing

--- package/qt5/qt5.mk
+++ package/qt5/qt5.mk
@@ -6,7 +6,7 @@
 
 QT5_VERSION_MAJOR = 5.15
 QT5_VERSION = $(QT5_VERSION_MAJOR).2
-QT5_SOURCE_TARBALL_PREFIX = everywhere-src
+QT5_SITE_BASE = https://invent.kde.org/qt/qt
 QT5_SITE = https://download.qt.io/archive/qt/$(QT5_VERSION_MAJOR)/$(QT5_VERSION)/submodules
 
 include $(sort $(wildcard package/qt5/*/*.mk))
