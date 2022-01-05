f50fddc54511f2514f60e5029ad9655473f01042
James Hilliard <james.hilliard1@gmail.com>
2022-01-05 02:48:20 -0700

    package/qt5/qt5webengine: bump to version 5.15.8
    We need to switch to the kde upstream as the existing upstream no
    longer updates the qt5 branch for most packages.
    
    We need to add a separate package for the chromium submodule so that
    we can apply patches for python3 support to it and replace catapult
    with a python3 compatible version.
    
    There are python3 patches available that apply to this version with
    minor modifications(to be added in a future patch).
    
    We need to set QT5WEBENGINE_SYNC_QT_HEADERS = YES as we are building
    from a repository archive instead of a release archive.
    
    We need to add some new required dependencies to qt5webengine so that
    it will build.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    ---
    Changes v3 -> v4:
      - linker fixes
    
    Changes v2 -> v3:
      - add kconfig entries

--- package/qt5/qt5webengine/qt5webengine.mk
+++ package/qt5/qt5webengine/qt5webengine.mk
@@ -5,18 +5,18 @@
 ################################################################################
 
 QT5WEBENGINE_VERSION = $(QT5_VERSION)
-QT5WEBENGINE_SITE = $(QT5_SITE)
-QT5WEBENGINE_SOURCE = qtwebengine-$(QT5_SOURCE_TARBALL_PREFIX)-$(QT5WEBENGINE_VERSION).tar.xz
+QT5WEBENGINE_SITE = $(QT5_SITE)/qtwebengine/-/archive/v$(QT5WEBENGINE_VERSION)-lts
+QT5WEBENGINE_SOURCE = qtwebengine-v$(QT5WEBENGINE_VERSION)-lts.tar.bz2
 QT5WEBENGINE_DEPENDENCIES = ffmpeg libglib2 libvpx libxkbcommon opus webp \
-	qt5declarative qt5webchannel host-bison host-flex host-gperf \
-	host-pkgconf host-python
+	qt5declarative qt5webchannel host-bison host-flex host-freetype \
+	host-gperf host-nodejs host-pkgconf host-python
+QT5WEBENGINE_PATCH_DEPENDENCIES = qt5webengine-chromium
 QT5WEBENGINE_INSTALL_STAGING = YES
-
-include package/qt5/qt5webengine/chromium-latest.inc
+QT5WEBENGINE_SYNC_QT_HEADERS = YES
 
 QT5WEBENGINE_LICENSE = GPL-2.0 or LGPL-3.0 or GPL-3.0 or GPL-3.0 with exception
 QT5WEBENGINE_LICENSE_FILES = LICENSE.GPL2 LICENSE.GPL3 LICENSE.GPL3-EXCEPT \
-	LICENSE.GPLv3 LICENSE.LGPL3 $(QT5WEBENGINE_CHROMIUM_LICENSE_FILES)
+	LICENSE.GPLv3 LICENSE.LGPL3
 
 ifeq ($(BR2_PACKAGE_QT5BASE_EXAMPLES),y)
 QT5WEBENGINE_DEPENDENCIES += qt5svg
@@ -24,7 +24,7 @@ endif
 
 ifeq ($(BR2_PACKAGE_QT5BASE_XCB),y)
 QT5WEBENGINE_DEPENDENCIES += xlib_libXScrnSaver xlib_libXcomposite \
-	xlib_libXcursor xlib_libXi xlib_libXrandr xlib_libXtst
+	xlib_libXcursor xlib_libXi xlib_libxkbfile xlib_libXrandr xlib_libXtst
 endif
 
 QT5WEBENGINE_DEPENDENCIES += host-libpng host-libnss libnss
@@ -41,24 +41,17 @@ else
 QT5WEBENGINE_CONF_OPTS += QT_CONFIG-=alsa
 endif
 
-# QtWebengine's build system uses python, but only supports python2. We work
-# around this by forcing python2 early in the PATH, via a python->python2
-# symlink.
-QT5WEBENGINE_ENV = PATH=$(@D)/host-bin:$(BR_PATH)
-define QT5WEBENGINE_PYTHON2_SYMLINK
-	mkdir -p $(@D)/host-bin
-	ln -sf $(HOST_DIR)/bin/python2 $(@D)/host-bin/python
-endef
-QT5WEBENGINE_PRE_CONFIGURE_HOOKS += QT5WEBENGINE_PYTHON2_SYMLINK
-
 QT5WEBENGINE_ENV += NINJAFLAGS="-j$(PARALLEL_JOBS)"
 
-define QT5WEBENGINE_CREATE_HOST_PKG_CONFIG
-	sed s%@HOST_DIR@%$(HOST_DIR)%g $(QT5WEBENGINE_PKGDIR)/host-pkg-config.in > $(@D)/host-bin/host-pkg-config
-	chmod +x $(@D)/host-bin/host-pkg-config
+define QT5WEBENGINE_COPY_CHROMIUM
+	rm -rf $(@D)/src/3rdparty
+	cp -a $(QT5WEBENGINE_CHROMIUM_DIR) $(@D)/src/3rdparty
 endef
-QT5WEBENGINE_PRE_CONFIGURE_HOOKS += QT5WEBENGINE_CREATE_HOST_PKG_CONFIG
-QT5WEBENGINE_ENV += GN_PKG_CONFIG_HOST=$(@D)/host-bin/host-pkg-config
+QT5WEBENGINE_POST_PATCH_HOOKS += QT5WEBENGINE_COPY_CHROMIUM
+
+QT5WEBENGINE_ENV += \
+	GN_PKG_CONFIG_HOST="$(HOST_DIR)/bin/pkgconf" \
+	GN_HOST_TOOLCHAIN_EXTRA_CPPFLAGS="$(HOST_CPPFLAGS) $(HOST_LDFLAGS)"
 
 QT5WEBENGINE_CONF_ENV = $(QT5WEBENGINE_ENV)
 QT5WEBENGINE_MAKE_ENV = $(QT5WEBENGINE_ENV)
