a2b1768c3d716ffa4c207087632610cb4caf0050
James Hilliard <james.hilliard1@gmail.com>
2024-01-09 22:37:10 -0700

    package/python-pymodbus: bump to version 3.6.4
    License hash changed due to year range update:
    https://github.com/pymodbus-dev/pymodbus/commit/131d30391efd9c64f1fec857ae0372746374e6d2
    
    Drop no longer required python-six runtime dependency.
    
    Drop no longer required python-serial runtime dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>

--- package/python-pymodbus/python-pymodbus.mk
+++ package/python-pymodbus/python-pymodbus.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYMODBUS_VERSION = 3.0.2
+PYTHON_PYMODBUS_VERSION = 3.6.4
 PYTHON_PYMODBUS_SOURCE = pymodbus-$(PYTHON_PYMODBUS_VERSION).tar.gz
-PYTHON_PYMODBUS_SITE = https://files.pythonhosted.org/packages/94/f7/6149a1b107fa54378a7adec83db7cd3679bf02216a6f5c83b37f55335897
+PYTHON_PYMODBUS_SITE = https://files.pythonhosted.org/packages/5b/7f/51eb8704092417ef8ab1d883ad793c27f7f34d67468efe36ca03c6c7863d
 PYTHON_PYMODBUS_SETUP_TYPE = setuptools
 PYTHON_PYMODBUS_LICENSE = BSD-3-Clause
 PYTHON_PYMODBUS_LICENSE_FILES = LICENSE
