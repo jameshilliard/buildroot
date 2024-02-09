c5e628e3154cfa06b7972c719c1ce5e6b543089a
James Hilliard <james.hilliard1@gmail.com>
2024-02-09 10:55:12 -0700

    package/python-cython: bump to version 3.0.8
    Tested by ensuring all packages with a host-python-cython dependency
    build succesfully.
    
    Note that due to namespace conflicts supporting both cython 0.29 and
    cython 3.0 variants at the same time would be difficult as both can
    not be installed at the same time.
    
    License hash changed due to changing links from http to https:
    https://github.com/cython/cython/commit/331d9d824ee5f0c539310332215ebd6ed3257325
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    ---
    Changes v2 -> v3:
      - add more details to commit message

--- package/python-cython/python-cython.mk
+++ package/python-cython/python-cython.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_CYTHON_VERSION = 0.29.36
+PYTHON_CYTHON_VERSION = 3.0.8
 PYTHON_CYTHON_SOURCE = Cython-$(PYTHON_CYTHON_VERSION).tar.gz
-PYTHON_CYTHON_SITE = https://files.pythonhosted.org/packages/38/db/df0e99d6c5fe19ee5c981d22aad557be4bdeed3ecfae25d47b84b07f0f98
+PYTHON_CYTHON_SITE = https://files.pythonhosted.org/packages/68/09/ffb61f29b8e3d207c444032b21328327d753e274ea081bc74e009827cc81
 PYTHON_CYTHON_SETUP_TYPE = setuptools
 PYTHON_CYTHON_LICENSE = Apache-2.0
 PYTHON_CYTHON_LICENSE_FILES = COPYING.txt LICENSE.txt
