c6969de85b6129623116a896138f4f6211a02e5b
James Hilliard <james.hilliard1@gmail.com>
2024-05-31 13:54:47 -0600

    package/vala: bump to version 0.56.17
    Add new host-gobject-introspection dependency.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>

--- package/vala/vala.mk
+++ package/vala/vala.mk
@@ -5,14 +5,18 @@
 ################################################################################
 
 VALA_VERSION_MAJOR = 0.56
-VALA_VERSION = $(VALA_VERSION_MAJOR).14
+VALA_VERSION = $(VALA_VERSION_MAJOR).17
 VALA_SITE = http://download.gnome.org/sources/vala/$(VALA_VERSION_MAJOR)
 VALA_SOURCE = vala-$(VALA_VERSION).tar.xz
 VALA_LICENSE = LGPL-2.1+
 VALA_LICENSE_FILES = COPYING
 VALA_CPE_ID_VENDOR = gnome
 
-HOST_VALA_DEPENDENCIES = host-bison host-flex host-libglib2
+HOST_VALA_DEPENDENCIES = \
+	host-bison \
+	host-flex \
+	host-gobject-introspection \
+	host-libglib2
 # Yes, the autoconf script understands ':' as "xsltproc is not
 # available".
 HOST_VALA_CONF_ENV = ac_cv_path_XSLTPROC=:
