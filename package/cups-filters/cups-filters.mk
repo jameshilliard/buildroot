d0a8cf693040b4ccb66471bcc6dbf410b75dc57f
James Hilliard <james.hilliard1@gmail.com>
2024-09-30 16:19:08 -0600

    package/cups-filters: bump to version 2.0.1
    Drop patch which is now upstream.
    
    Project relicensed to Apache-2.0 with GPL-2.0/LGPL-2.0 exception:
    https://github.com/OpenPrinting/cups-filters/commit/14f11135d8aa25050e8627354b2c1f9984f7e3a3
    
    Add new libcupsfilters dependency.
    
    Add new libppd dependency.
    
    Drop no longer required libglib2 dependency.
    
    Drop no longer required lcms2 dependency.
    
    Drop no longer required qpdf dependency.
    
    Drop no longer required fontconfig dependency.
    
    Drop no longer required freetype dependency.
    
    Drop no longer required jpeg dependency.
    
    Add new --enable-pstops config option.
    
    Add new --enable-rastertopwg config option.
    
    Add new --with-shell=/bin/sh config option.
    
    Remove no longer supported --disable-braille config option.
    
    Remove no longer supported --with-pdftops config option.
    
    Remove no longer supported --with-jpeg config option.
    
    Remove no longer supported --with-test-font-path config option.
    
    Remove no longer supported --without-rcdir config option.
    
    Remove no longer supported --with-png config option and dependency.
    
    Remove no longer supported --with-tiff config option and dependency.
    
    Remove no longer supported --enable-dbus config option and dependency.
    
    Remove no longer supported --enable-avahi config option and
    dependency.
    
    Remove no longer supported --enable-ijs config option and dependency.
    
    Remove no longer supported --enable-exif config option and dependency.
    
    Remove cups-browsed service installation as cups-browsed is no longer
    part of cups-filters.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>

--- package/cups-filters/cups-filters.mk
+++ package/cups-filters/cups-filters.mk
@@ -4,56 +4,24 @@
 #
 ################################################################################
 
-CUPS_FILTERS_VERSION = 1.28.17
+CUPS_FILTERS_VERSION = 2.0.1
+CUPS_FILTERS_SOURCE = cups-filters-$(CUPS_FILTERS_VERSION).tar.xz
 CUPS_FILTERS_SITE = https://github.com/OpenPrinting/cups-filters/releases/download/$(CUPS_FILTERS_VERSION)
-CUPS_FILTERS_LICENSE = GPL-2.0, GPL-2.0+, GPL-3.0, GPL-3.0+, LGPL-2, LGPL-2.1+, MIT, BSD-4-Clause
-CUPS_FILTERS_LICENSE_FILES = COPYING
+CUPS_FILTERS_LICENSE = Apache-2.0 with GPL-2.0/LGPL-2.0 exception
+CUPS_FILTERS_LICENSE_FILES = COPYING LICENSE
 CUPS_FILTERS_CPE_ID_VENDOR = linuxfoundation
 
-# 0001-beh-backend-Use-execv-instead-of-system-CVE-2023-24805.patch
-CUPS_FILTERS_IGNORE_CVES += CVE-2023-24805
-
-CUPS_FILTERS_DEPENDENCIES = cups libglib2 lcms2 qpdf fontconfig freetype jpeg
+CUPS_FILTERS_DEPENDENCIES = cups libcupsfilters libppd
 
 CUPS_FILTERS_CONF_OPTS = \
 	--disable-mutool \
 	--disable-foomatic \
-	--disable-braille \
+	--enable-pstops \
+	--enable-rastertopwg \
 	--enable-imagefilters \
+	--with-shell=/bin/sh \
 	--with-cups-config=$(STAGING_DIR)/usr/bin/cups-config \
-	--with-sysroot=$(STAGING_DIR) \
-	--with-pdftops=pdftops \
-	--with-jpeg \
-	--with-test-font-path=/dev/null \
-	--without-rcdir
-
-ifeq ($(BR2_PACKAGE_LIBPNG),y)
-CUPS_FILTERS_CONF_OPTS += --with-png
-CUPS_FILTERS_DEPENDENCIES += libpng
-else
-CUPS_FILTERS_CONF_OPTS += --without-png
-endif
-
-ifeq ($(BR2_PACKAGE_TIFF),y)
-CUPS_FILTERS_CONF_OPTS += --with-tiff
-CUPS_FILTERS_DEPENDENCIES += tiff
-else
-CUPS_FILTERS_CONF_OPTS += --without-tiff
-endif
-
-ifeq ($(BR2_PACKAGE_DBUS),y)
-CUPS_FILTERS_CONF_OPTS += --enable-dbus
-CUPS_FILTERS_DEPENDENCIES += dbus
-else
-CUPS_FILTERS_CONF_OPTS += --disable-dbus
-endif
-
-ifeq ($(BR2_PACKAGE_AVAHI_LIBAVAHI_CLIENT),y)
-CUPS_FILTERS_DEPENDENCIES += avahi
-CUPS_FILTERS_CONF_OPTS += --enable-avahi
-else
-CUPS_FILTERS_CONF_OPTS += --disable-avahi
-endif
+	--with-sysroot=$(STAGING_DIR)
 
 ifeq ($(BR2_PACKAGE_GHOSTSCRIPT),y)
 CUPS_FILTERS_DEPENDENCIES += ghostscript
@@ -62,35 +30,11 @@ else
 CUPS_FILTERS_CONF_OPTS += --disable-ghostscript
 endif
 
-ifeq ($(BR2_PACKAGE_IJS),y)
-CUPS_FILTERS_DEPENDENCIES += ijs
-CUPS_FILTERS_CONF_OPTS += --enable-ijs
-else
-CUPS_FILTERS_CONF_OPTS += --disable-ijs
-endif
-
 ifeq ($(BR2_PACKAGE_POPPLER),y)
 CUPS_FILTERS_DEPENDENCIES += poppler
 CUPS_FILTERS_CONF_OPTS += --enable-poppler
 else
 CUPS_FILTERS_CONF_OPTS += --disable-poppler
 endif
-
-ifeq ($(BR2_PACKAGE_LIBEXIF),y)
-CUPS_FILTERS_CONF_OPTS += --enable-exif
-CUPS_FILTERS_DEPENDENCIES += libexif
-else
-CUPS_FILTERS_CONF_OPTS += --disable-exif
-endif
-
-define CUPS_FILTERS_INSTALL_INIT_SYSV
-	$(INSTALL) -D -m 0755 package/cups-filters/S82cups-browsed \
-		$(TARGET_DIR)/etc/init.d/S82cups-browsed
-endef
-
-define CUPS_FILTERS_INSTALL_INIT_SYSTEMD
-	$(INSTALL) -D -m 0755 $(@D)/utils/cups-browsed.service \
-		$(TARGET_DIR)/usr/lib/systemd/system/cups-browsed.service
-endef
 
 $(eval $(autotools-package))
